5d8d3b5ea24e0e6541e1dfd9bc91ab6a2e904b0e
kanchanproseth <kanchanproseth@users.noreply.github.com>
2017-10-27 13:00:20 +0700

    test POST and GET with flask

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_restful import Api
+from flask_restful_swagger import swagger
 
 app = Flask(__name__)
-
 store = [
     {
         'name': 'Sovanna Supper Market',
@@ -42,14 +43,27 @@ store = [
 ]
 
 # POST /store data: {name:}
-@app.route('/store', methods = ['POST'])
+
+
+@app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'item': []
+    }
+    store.append(new_store)
+    return jsonify(new_store)
 
 # GET /store/<string:name>
+
+
 @app.route('/store/<string:name>')
 def get_store_by_name(name):
-    pass
+    for each_store in store:
+        if each_store['name'] == name:
+            return jsonify(each_store)
+    return jsonify({'message': 'store not found'})
 
 # GET /store
 @app.route('/store')
@@ -57,19 +71,32 @@ def get_store():
     return jsonify({'store': store})
 
 # POST /store/<string:name>/item {name: , price:}
-@app.route('/store/<string:name>/item', methods = ['POST'])
+@app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for each_store in store:
+        if each_store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            each_store['item'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'store not found'})
 
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
 def get_item_in_store(name):
-    pass
+    for each_store in store:
+        if each_store['name'] == name:
+            return jsonify({'items': each_store['item']})
+    return jsonify({'message': 'store not found'})
+
 
 @app.route('/')
-#http://www.google.com/ 
-
+#http://www.google.com/
 def home():
     return "Welcome Home"
+
 
 app.run(port=5000)
